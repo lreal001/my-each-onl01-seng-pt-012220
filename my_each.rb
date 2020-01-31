c1eade86e83a851314e28bc74c9dc4b31ad8ff2d
Lessllie Real <rlessllie@yahoo.com>
2020-01-31 12:28:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+
+
+array = []
+def my_each(array) do |i|
+                   
+  i = 0 
+  
+  while i < array.length 
+  yield (array[i])
+  i = i + 1
+  p i
+  end 
+  array
 end
+
